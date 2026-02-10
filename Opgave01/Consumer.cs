aa3251a081848d46be005d4de06d2bc7e87b01fd
Karsten <karsten-kirkegaard@hotmail.com>
2026-02-10 09:57:34 +0100

    opgave01 færdig

---
+++ Opgave01/Consumer.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace Opgave01;
+
+public class Consumer {
+    public async Task StartListening() {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost"
+        };
+        var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: "person_queue", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (model, ea) => {
+            var body = ea.Body.ToArray();
+            var json = Encoding.UTF8.GetString(body);
+            Person? modtaget = JsonSerializer.Deserialize<Person>(json);
+
+            Console.WriteLine($"[Consumer] Modtaget og deserialiseret: {json}");
+            await Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(queue: "person_queue", autoAck: true, consumer: consumer);
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Opgave01/Producer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace Opgave01;
+
+public class Producer {
+
+    public async Task SendPerson(Person person) {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost"
+        };
+        
+        var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+        
+        await channel.QueueDeclareAsync(queue: "person_queue", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+        
+        string json = JsonSerializer.Serialize(person);
+        var body = Encoding.UTF8.GetBytes(json);
+        
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "person_queue", body: body);
+        Console.WriteLine($"[Producer] Sendt og serialiseret: {json}");
+    }
+}
